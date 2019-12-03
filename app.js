19ca1b43e796579d56eb9d6a1be1dc7b36f58348
Anugrah Lambogo <lambogoai@gmail.com>
2019-12-03 15:36:04 -0600

    added filtering functionality

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+/* eslint-disable indent */
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const movieData = require('./movie-data.js');
 
-console.log(process.env.API_TOKEN);
+//console.log(process.env.API_TOKEN);
 const app = express();
 
 app.use(morgan('dev'));
@@ -16,9 +18,43 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
 
+
+    console.log(apiToken);
+    console.log(authToken);
+
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized Request' })
     }
     next();
 })
 
+
+app.get('/movie', function getMovie(req, res) {
+    let movieResults = movieData;
+
+    if (req.query.genre) {
+        movieResults.filter(movie => {
+            movie.genre.toLowerCase().includes(`${req.query.genre}`);
+        });
+    }
+
+    if (req.query.country) {
+        movieResults.filter(movie => {
+            movie.country.toLowerCase().includes(`${req.query.country}`);
+        })
+    }
+
+    if (req.query.avg_vote) {
+        movieResults.filter(movie => {
+            let vote = Number(req.query.avg_vote);
+            let avgVote = Number(movie.avg_vote);
+            avgVote >= vote;
+        })
+    }
+
+})
+
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000!!');
+});
