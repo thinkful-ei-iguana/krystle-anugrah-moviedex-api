d1de752e9710854e3b2661e2fa0460af99303964
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2020-03-04 18:57:03 -0500

    update

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 /* eslint-disable indent */
 require('dotenv').config();
 const express = require('express');
-const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
-app.use(morgan(morganSetting))
 const cors = require('cors');
 const helmet = require('helmet');
 const movieData = require('./movie-data.json');
@@ -10,7 +8,8 @@ const movieData = require('./movie-data.json');
 //console.log(process.env.API_TOKEN);
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
 
@@ -29,42 +28,42 @@ app.use(function validateBearerToken(req, res, next) {
 })
 
 
-app.get('/movie', function getMovie(req, res) {
-    let movieResults = movieData;
+app.get('/movie', function handleGetMovie(req, res) {
+  let response = MOVIES;
 
-    if (req.query.genre) {
-     return   movieResults = movieResults.filter(movie => {
-            movie.genre.toLowerCase().includes(req.query.genre);
-        });
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
 
-    }
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
 
-    if (req.query.country) {
-     return   movieResults = movieResults.filter(movie => {
-            movie.country.toLowerCase().includes(req.query.country);
-        })
-    }
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    )
+  }
 
-    if (req.query.avg_vote) {
-        movieResults = movieResults.filter(movie => {
-            let vote = Number(req.query.avg_vote);
-      return  let avgVote = Number(movie.avg_vote);
-            avgVote >= vote;
-        })
-    }
+  res.json(response)
+})
+
     
     res.json(movieResults);
 })
 
-app.use((error, req, res, next) => {
-    let response
-    if (process.env.NODE_ENV === 'production') {
-      response = { error: { message: 'server error' }}
-    } else {
-      response = { error }
-    }
-    res.status(500).json(response)
-  })
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+})
 
 const PORT = process.env.PORT || 8000
 
